d43ffb13dcbc49e580df4bb645d4d7f1ded621b4
Jordan Terry <jordan.terry@theguardian.com>
2019-04-04 16:30:49 +0100

    Added: ConstraintLayout and a splash screen.

---
+++ app/src/main/java/com/guardian/go/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.guardian.go
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
