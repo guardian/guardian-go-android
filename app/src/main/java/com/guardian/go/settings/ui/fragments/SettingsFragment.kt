bda5749abda7126aeb18035a6ea19938c37e0f27
Jordan Terry <jordan.terry@theguardian.com>
2019-04-08 16:05:54 +0100

    Add the saved time to the preferences screen.

--- app/src/main/java/com/guardian/go/settings/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/guardian/go/settings/ui/fragments/SettingsFragment.kt
@@ -1,19 +1,53 @@
 package com.guardian.go.settings.ui.fragments
 
+import android.app.TimePickerDialog
 import android.os.Bundle
+import android.widget.TimePicker
+import androidx.lifecycle.Observer
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import androidx.preference.SwitchPreferenceCompat
 import com.guardian.go.R
+import com.guardian.go.settings.ui.viewmodel.SettingsViewModel
+import com.guardian.go.time.ui.data.SharedPreferencesTimeRepository
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : PreferenceFragmentCompat(), TimePickerDialog.OnTimeSetListener {
+
+    private lateinit var settingsViewModel: SettingsViewModel
+
+    private lateinit var timePickerDialog: TimePickerDialog
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
-        (findPreference("pref_dark_mode") as SwitchPreferenceCompat).setOnPreferenceChangeListener { preference, newValue ->
+        timePickerDialog = TimePickerDialog(requireContext(), this, 0, 0, true)
+        settingsViewModel = SettingsViewModel(
+            SharedPreferencesTimeRepository(
+                PreferenceManager.getDefaultSharedPreferences(
+                    requireContext()
+                )
+            )
+        )
+        settingsViewModel.model.observe(this, Observer { model ->
+            if (model != null) {
+                findPreference("pref_schedule_time").summary =
+                    String.format("%02d:%02d", model.time.hour, model.time.minute)
+                timePickerDialog = TimePickerDialog(requireContext(), this, model.time.hour, model.time.minute, true)
+            }
+        })
+        settingsViewModel.load()
+        (findPreference("pref_dark_mode") as SwitchPreferenceCompat).setOnPreferenceChangeListener { _, newValue ->
             if (newValue is Boolean) {
                 requireActivity().recreate()
             }
             true
         }
+        findPreference("pref_schedule_time").setOnPreferenceClickListener {
+            timePickerDialog.show()
+            true
+        }
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        settingsViewModel.saveTime(hourOfDay, minute)
     }
 }
---
+++ app/src/main/java/com/guardian/go/settings/ui/viewmodel/SettingsViewModel.kt
@@ -0,0 +1,54 @@
+package com.guardian.go.settings.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.guardian.go.time.ui.data.Time
+import com.guardian.go.time.ui.data.TimeRepository
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class SettingsViewModel(
+    private val timeRepository: TimeRepository
+) : ViewModel() {
+
+    private val mutableModel: MutableLiveData<Model> = MutableLiveData()
+
+    val model: LiveData<Model> = mutableModel
+
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
+
+    fun load() {
+        compositeDisposable.add(Single.fromCallable(timeRepository::getTime)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { time ->
+                mutableModel.postValue(
+                    Model(
+                        time = time
+                    )
+                )
+            })
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
+
+    fun saveTime(hourOfDay: Int, minute: Int) {
+        val time = Time(hourOfDay, minute)
+        timeRepository.saveTime(time)
+        mutableModel.postValue(
+            Model(
+                time = time
+            )
+        )
+    }
+
+    data class Model(
+        val time: Time
+    )
+}
