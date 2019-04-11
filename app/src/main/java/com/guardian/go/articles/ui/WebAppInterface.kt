88a68e0b084addb12f589e1e37bff6536b3399c3
Jamie Webb <jamie.webb@guardian.co.uk>
2019-04-11 16:05:31 +0100

    article percentages

--- app/src/main/java/com/guardian/go/articles/ui/WebAppInterface.kt
+++ app/src/main/java/com/guardian/go/articles/ui/WebAppInterface.kt
@@ -4,20 +4,18 @@ import android.content.Context
 import android.webkit.JavascriptInterface
 import android.widget.Toast
 
-/** Instantiate the interface and set the context  */
 class WebAppInterface(
     private val context: Context,
     private val onCloseClick: () -> Unit
 ) {
 
-    /** Show a toast from the web page  */
     @JavascriptInterface
     fun showToast(toast: String) {
         Toast.makeText(context, toast, Toast.LENGTH_SHORT).show()
     }
 
     @JavascriptInterface
-    fun updateProgress(percentage: String, articleId: String) {}
+    fun updateProgress(percentage: String) {}
 
     @JavascriptInterface
     fun close() {
